fba8a22c284909a2bc8e299aba67d64224c27c6e
Thero <pethethcjc025@student.wethinkcode.co.za>
2025-07-14 09:22:53 +0200

    new testcase: TestParseArgs.test_parse_args_defaults

--- tests/testmv.py
+++ tests/testmv.py
@@ -5,21 +5,32 @@ import sys
 from mvbuild.src.main import *
 
 class TestParseArgs(unittest.TestCase):
-    @patch.object(sys, 'argv', ['mv', 'build', '--id', 'com.example', '--n', 'my-app', '--f', 'false'])
+    @patch.object(sys, 'argv', ['mv', 'build', '--id', 'com.example', '--n', 'my-app', '--i', 'false'])
     def test_parse_args_valid(self):
         args = parse_args()
         #
         self.assertEqual(args.command, 'build')
         self.assertEqual(args.id, 'com.example')
-        self.assertEqual(args.n, 'mu-app')
+        self.assertEqual(args.n, 'my-app')
         self.assertIn(args.i, ['f', 'false'])
 
     
-    @patch.object(sys, 'argv', ['mv', 'build', '--id', 'com.example', '--n', 'my-app', '--f', 'true'])
+    @patch.object(sys, 'argv', ['mv', 'build', '--id', 'com.example', '--n', 'my-app', '--i', 'true'])
     def test_parse_args_valid_2(self):
         args = parse_args()
         #
         self.assertEqual(args.command, 'build')
         self.assertEqual(args.id, 'com.example')
-        self.assertEqual(args.n, 'mu-app')
+        self.assertEqual(args.n, 'my-app')
         self.assertIn(args.i, ['t', 'true'])
+
+
+    @patch.object(sys, 'argv', ['mv', 'build', '--id', 'com.example', '--n', 'my-app'])
+    def test_parse_args_defaults(self):
+        args = parse_args()
+        #
+        self.assertIn(args.i, ['f', 'false'])
+
+
+if __name__ == '__main__':
+    unittest.main()
